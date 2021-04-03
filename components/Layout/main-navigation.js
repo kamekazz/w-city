15842bc33059e042125a46824186a73bb256b778
kamekazz <taveras78@gmail.com>
2021-04-02 22:31:29 -0400

    logout fonction

--- components/Layout/main-navigation.js
+++ components/Layout/main-navigation.js
@@ -1,11 +1,16 @@
 import Link from 'next/link';
-
+import { useDispatch } from 'react-redux';
 import classes from './main-navigation.module.css';
 
 function MainNavigation() {
+  const dispatch = useDispatch();
+  const logout = () => {
+    dispatch({ type: 'LOGOUT' });
+  };
+
   return (
     <header className={classes.header}>
-      <Link href='/'>
+      <Link href="/">
         <a>
           <div className={classes.logo}>Next Auth</div>
         </a>
@@ -13,13 +18,13 @@ function MainNavigation() {
       <nav>
         <ul>
           <li>
-            <Link href='/auth'>Login</Link>
+            <Link href="/auth">Login</Link>
           </li>
           <li>
-            <Link href='/profile'>Profile</Link>
+            <Link href="/profile">Profile</Link>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={logout}>Logout</button>
           </li>
         </ul>
       </nav>
