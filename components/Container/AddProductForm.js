de2af49293cae083e4287da66dff446b6730d9fc
kamekazz <taveras78@gmail.com>
2021-04-09 17:47:22 -0400

    add ms LWH

--- components/Container/AddProductForm.js
+++ components/Container/AddProductForm.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Button, makeStyles, Paper } from '@material-ui/core';
+import { Button, Container, makeStyles, Paper } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormLabel from '@material-ui/core/FormLabel';
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  rootForm: {
     '& > *': {
       margin: theme.spacing(1),
     },
@@ -18,6 +18,18 @@ const useStyles = makeStyles((theme) => ({
     padding: '1rem',
     '& > *': {
       width: '10rem',
+      margin: theme.spacing(1),
+    },
+  },
+  msContainer: {
+    '& > *': {
+      margin: theme.spacing(1),
+      padding: theme.spacing(1),
+      display: 'flex',
+    },
+  },
+  BoxMeasure: {
+    '& > *': {
       margin: theme.spacing(1),
     },
   },
@@ -56,159 +68,148 @@ const useStyles = makeStyles((theme) => ({
 
 export default function AddProductForm() {
   const classes = useStyles();
-  const [value, setValue] = React.useState('Lodging');
 
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  };
   return (
-    <form className={classes.root} autoComplete="off">
-      <Paper className={classes.searchFrom}>
-        <TextField
-          name="ibm"
-          label="IBM"
-          variant="outlined"
-          autoFocus
-          inputProps={{ size: 20 }}
-          value={'123456'}
-          InputProps={{
-            classes: {
-              input: classes.textInputIbm,
-            },
-          }}
-        />
-        <Button variant="contained">cancel</Button>
-        <Button variant="contained" color="primary">
-          add load
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          style={{ color: 'white' }}
-        >
-          add new product
-        </Button>
-      </Paper>
-      <Paper className={classes.importantForm}>
-        <TextField
-          name="alias"
-          label="Alias"
-          variant="outlined"
-          InputProps={{
-            classes: {
-              input: classes.textInput,
-            },
-          }}
-        />
-        <TextField
-          name="Count"
-          label="count"
-          variant="outlined"
-          InputProps={{
-            classes: {
-              input: classes.textInput,
-            },
-          }}
-        />
-        <TextField
-          name="msCount"
-          label="MS count"
-          variant="outlined"
-          InputProps={{
-            classes: {
-              input: classes.textInput,
-            },
-          }}
-        />
-      </Paper>
-      <Paper className={classes.sizeForm}>
-        <TextField
-          name="plCount"
-          label="PL count"
-          variant="outlined"
-          InputProps={{
-            classes: {
-              input: classes.textInput,
-            },
-          }}
-        />
-        <TextField
-          name="plTi"
-          label="PL Ti"
-          variant="outlined"
-          InputProps={{
-            classes: {
-              input: classes.textInput,
-            },
-          }}
-        />
-        <TextField
-          name="plHi"
-          label="PL Hi"
-          variant="outlined"
-          InputProps={{
-            classes: {
-              input: classes.textInput,
-            },
-          }}
-        />
-        <TextField
-          name="p1Count"
-          label="P1 count"
-          variant="outlined"
-          InputProps={{
-            classes: {
-              input: classes.textInput,
-            },
-          }}
-        />
-        <TextField
-          name="p1Ti"
-          label="P1 Ti"
-          variant="outlined"
-          InputProps={{
-            classes: {
-              input: classes.textInput,
-            },
-          }}
-        />
-        <TextField
-          name="p1Hi"
-          label="P1 Hi"
-          variant="outlined"
-          InputProps={{
-            classes: {
-              input: classes.textInput,
-            },
-          }}
-        />
-      </Paper>
-      <Paper className={classes.statusForm}>
-        <FormLabel component="legend">Status</FormLabel>
-        <RadioGroup
-          aria-label="gender"
-          name="gender1"
-          value={value}
-          onChange={handleChange}
-        >
-          <FormControlLabel
-            value="Lodging"
-            control={<Radio />}
-            label="LODGING"
+    <Container className={classes.root}>
+      <form autoComplete="off" className={classes.rootForm}>
+        <Paper className={classes.searchFrom}>
+          <TextField
+            name="ibm"
+            label="IBM"
+            variant="outlined"
+            autoFocus
+            inputProps={{ size: 20 }}
+            value={'123456'}
+            InputProps={{
+              classes: {
+                input: classes.textInputIbm,
+              },
+            }}
           />
-          <FormControlLabel
-            value="standby"
-            control={<Radio />}
-            label="STANDBY"
+          <Button variant="contained">cancel</Button>
+          <Button variant="contained" color="primary">
+            add load
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            style={{ color: 'white' }}
+          >
+            add new product
+          </Button>
+        </Paper>
+        <Paper className={classes.importantForm}>
+          <TextField
+            name="alias"
+            label="Alias"
+            variant="outlined"
+            InputProps={{
+              classes: {
+                input: classes.textInput,
+              },
+            }}
           />
-          <FormControlLabel
-            value="done"
-            disabled
-            control={<Radio />}
-            label="DONE"
+          <TextField
+            name="Count"
+            label="count"
+            variant="outlined"
+            InputProps={{
+              classes: {
+                input: classes.textInput,
+              },
+            }}
           />
-        </RadioGroup>
-      </Paper>
-    </form>
+          <TextField
+            name="msCount"
+            label="MS count"
+            variant="outlined"
+            InputProps={{
+              classes: {
+                input: classes.textInput,
+              },
+            }}
+          />
+        </Paper>
+        <Paper className={classes.sizeForm}>
+          <TextField
+            name="plCount"
+            label="PL count"
+            variant="outlined"
+            InputProps={{
+              classes: {
+                input: classes.textInput,
+              },
+            }}
+          />
+          <TextField
+            name="plTi"
+            label="PL Ti"
+            variant="outlined"
+            InputProps={{
+              classes: {
+                input: classes.textInput,
+              },
+            }}
+          />
+          <TextField
+            name="plHi"
+            label="PL Hi"
+            variant="outlined"
+            InputProps={{
+              classes: {
+                input: classes.textInput,
+              },
+            }}
+          />
+          <TextField
+            name="p1Count"
+            label="P1 count"
+            variant="outlined"
+            InputProps={{
+              classes: {
+                input: classes.textInput,
+              },
+            }}
+          />
+          <TextField
+            name="p1Ti"
+            label="P1 Ti"
+            variant="outlined"
+            InputProps={{
+              classes: {
+                input: classes.textInput,
+              },
+            }}
+          />
+          <TextField
+            name="p1Hi"
+            label="P1 Hi"
+            variant="outlined"
+            InputProps={{
+              classes: {
+                input: classes.textInput,
+              },
+            }}
+          />
+        </Paper>
+      </form>
+      <div className={classes.msContainer}>
+        <BoxMeasure />
+      </div>
+    </Container>
   );
 }
+
+const BoxMeasure = () => {
+  const classes = useStyles();
+  return (
+    <Paper component={'form'} className={classes.BoxMeasure}>
+      <TextField name="msLength" label="MS Length" variant="outlined" />
+      <TextField name="msWidth" label="MS Width" variant="outlined" />
+      <TextField name="msHeight" label="MS Height" variant="outlined" />
+      <Button variant="contained" color="primary">
+        save
+      </Button>
+    </Paper>
+  );
+};
