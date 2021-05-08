24546c0a805245d7b9d026f0bf094526e122532d
kamekazz <taveras78@gmail.com>
2021-05-07 20:52:17 -0400

    test the https line

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== 'production'
     ? 'http://localhost:3000'
-    : 'http://w-city.herokuapp.com';
+    : 'https://w-city.herokuapp.com';
 
 module.exports = baseUrl;
