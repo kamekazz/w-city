bb821793884d7f966662613b595dbd86abe3c383
kamekazz <taveras78@gmail.com>
2021-05-09 14:37:31 -0400

    add product form ui done

--- components/Reveiving/ContainerInfo/addProduct/AddProductToContainerForm.jsx
+++ components/Reveiving/ContainerInfo/addProduct/AddProductToContainerForm.jsx
@@ -8,7 +8,11 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormLabel from '@material-ui/core/FormLabel';
 import { useDispatch, useSelector } from 'react-redux';
-import { acIsNewProduct } from '../../../../redux/containerAdmin';
+import {
+  acIsNewProduct,
+  acResetProductInfo,
+  acSaveUOM,
+} from '../../../../redux/containerAdmin';
 const transferOption = [
   {
     value: 'freeport',
@@ -26,9 +30,21 @@ const transferOption = [
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+    },
+  },
+  ibmForm: {
+    display: 'flex',
     flexWrap: 'wrap',
-    marginBottom: theme.spacing(1),
+    marginRight: theme.spacing(1),
     maxWidth: 452,
+    [theme.breakpoints.down('xs')]: {
+      marginRight: 0,
+      marginBottom: theme.spacing(1),
+    },
   },
   textFields: {
     display: 'flex',
@@ -143,87 +159,286 @@ export default function AddProductToContainerForm() {
     clearInput();
   };
   return (
-    <Paper className={classes.root} component={'form'} onSubmit={handelSubmit}>
-      <div className={classes.textFields}>
-        <TextField
-          name="ibm"
-          label="IBM"
-          variant="outlined"
-          inputRef={inputRef}
-          autoFocus
-          onInput={(e) => {
-            e.target.value = Math.max(0, parseInt(e.target.value))
-              .toString()
-              .slice(0, 6);
-          }}
-          min={0}
-          type="number"
-          value={inputs.ibm}
-          onChange={onChange}
-          className={classes.textField}
-          margin="dense"
-          variant="filled"
-        />
-        <TextField
-          name="alias"
-          label="Alias"
-          variant="outlined"
-          className={classes.textField}
-          type="text"
-          value={inputs.alias}
-          onChange={onChange}
-          margin="dense"
-          variant="filled"
-        />
-        <TextField
-          name="totalCount"
-          label="Total Count"
-          variant="outlined"
-          type="number"
-          value={inputs.totalCount}
-          onChange={onChange}
-          margin="dense"
-          variant="filled"
-          className={classes.textField}
-        />
-
-        {stationContainer.isTransfer === 'yes' && (
+    <div className={classes.root}>
+      <Paper
+        className={classes.ibmForm}
+        component={'form'}
+        onSubmit={handelSubmit}
+      >
+        <div className={classes.textFields}>
           <TextField
-            select
-            label="Transfers Locations"
-            value={stationContainer.transfer}
+            name="ibm"
+            label="IBM"
+            variant="outlined"
+            inputRef={inputRef}
+            autoFocus
+            onInput={(e) => {
+              e.target.value = Math.max(0, parseInt(e.target.value))
+                .toString()
+                .slice(0, 6);
+            }}
+            min={0}
+            type="number"
+            value={inputs.ibm}
+            onChange={onChange}
             className={classes.textField}
-            onChange={handleChangeTransfer}
             margin="dense"
             variant="filled"
-            value={transfer}
-          >
-            {transferOption.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
-        )}
+          />
+          <TextField
+            name="alias"
+            label="Alias"
+            variant="outlined"
+            className={classes.textField}
+            type="text"
+            value={inputs.alias}
+            onChange={onChange}
+            margin="dense"
+            variant="filled"
+          />
+          <TextField
+            name="totalCount"
+            label="Total Count"
+            variant="outlined"
+            type="number"
+            value={inputs.totalCount}
+            onChange={onChange}
+            margin="dense"
+            variant="filled"
+            className={classes.textField}
+          />
 
-        <Button
-          variant="contained"
-          color="primary"
-          disabled={disabledFormButton}
-          onClick={addLoad}
-          className={classes.button}
-        >
-          add load
-        </Button>
-        <Button
-          variant="contained"
-          onClick={clearInput}
-          className={classes.button}
-          disabled={disabledFormButton}
-        >
-          cancel
-        </Button>
-      </div>
-    </Paper>
+          {stationContainer.isTransfer === 'yes' && (
+            <TextField
+              select
+              label="Transfers Locations"
+              value={stationContainer.transfer}
+              className={classes.textField}
+              onChange={handleChangeTransfer}
+              margin="dense"
+              variant="filled"
+              value={transfer}
+            >
+              {transferOption.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          )}
+
+          <Button
+            variant="contained"
+            color="primary"
+            disabled={disabledFormButton}
+            onClick={addLoad}
+            className={classes.button}
+          >
+            add load
+          </Button>
+          <Button
+            variant="contained"
+            onClick={clearInput}
+            className={classes.button}
+            disabled={disabledFormButton}
+          >
+            cancel
+          </Button>
+        </div>
+      </Paper>
+      {stationProduct?.ibm && (
+        <UOMandSizeComponent key={stationProduct?.updatedAt} />
+      )}
+    </div>
   );
 }
+
+const useStylesUOM = makeStyles((theme) => ({
+  from: {
+    display: 'grid',
+    padding: '1rem',
+    gridTemplateColumns: '1fr 1fr 1fr',
+    gridTemplateRows: '1fr 1fr 1fr 2fr',
+    gap: '1rem',
+  },
+  itemPalletConfig: {
+    placeSelf: 'center',
+  },
+}));
+//UOMandSizeComponent
+const UOMandSizeComponent = () => {
+  const classes = useStylesUOM();
+  const { stationProduct } = useSelector((state) => state.containerAdmin);
+  const [inputs, setInputs] = useState(stationProduct);
+  const dispatch = useDispatch();
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+    setInputs((prevState) => {
+      return {
+        ...prevState,
+        [inputName]: newValue,
+      };
+    });
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(acSaveUOM(inputs));
+  };
+
+  const onReset = () => {
+    dispatch(acResetProductInfo());
+  };
+
+  return (
+    <Paper className={classes.from} component="form" onSubmit={onSubmit}>
+      <TextField
+        name="plUOM"
+        label="PL UOM"
+        variant="outlined"
+        onChange={handleChange}
+        value={inputs?.plUOM}
+        onInput={(e) => {
+          e.target.value = Math.max(0, parseInt(e.target.value))
+            .toString()
+            .slice(0, 8);
+        }}
+        min={0}
+        type="number"
+      />
+      <TextField
+        name="plTi"
+        label="PL Ti"
+        variant="outlined"
+        onChange={handleChange}
+        value={inputs?.plTi}
+        onInput={(e) => {
+          e.target.value = Math.max(0, parseInt(e.target.value))
+            .toString()
+            .slice(0, 8);
+        }}
+        min={0}
+        type="number"
+      />
+      <TextField
+        name="plHi"
+        label="PL Hi"
+        variant="outlined"
+        onChange={handleChange}
+        value={inputs?.plHi}
+        onInput={(e) => {
+          e.target.value = Math.max(0, parseInt(e.target.value))
+            .toString()
+            .slice(0, 8);
+        }}
+        min={0}
+        type="number"
+      />
+
+      <TextField
+        name="p1UOM"
+        label="P1 UOM"
+        variant="outlined"
+        onChange={handleChange}
+        value={inputs?.p1UOM}
+        onInput={(e) => {
+          e.target.value = Math.max(0, parseInt(e.target.value))
+            .toString()
+            .slice(0, 8);
+        }}
+        min={0}
+        type="number"
+      />
+      <TextField
+        name="p1Ti"
+        label="P1 Ti"
+        variant="outlined"
+        onChange={handleChange}
+        value={inputs?.p1Ti}
+        onInput={(e) => {
+          e.target.value = Math.max(0, parseInt(e.target.value))
+            .toString()
+            .slice(0, 8);
+        }}
+        min={0}
+        type="number"
+      />
+      <TextField
+        name="p1Hi"
+        label="P1 Hi"
+        variant="outlined"
+        onChange={handleChange}
+        value={inputs?.p1Hi}
+        onInput={(e) => {
+          e.target.value = Math.max(0, parseInt(e.target.value))
+            .toString()
+            .slice(0, 8);
+        }}
+        min={0}
+        type="number"
+      />
+      <TextField
+        name="msUOM"
+        label="MS UOM"
+        variant="outlined"
+        onChange={handleChange}
+        value={inputs?.msUOM}
+        onInput={(e) => {
+          e.target.value = Math.max(0, parseInt(e.target.value))
+            .toString()
+            .slice(0, 8);
+        }}
+        min={0}
+        type="number"
+      />
+      <TextField
+        name="plMaxHeight"
+        label="Max PL Height"
+        variant="outlined"
+        onChange={handleChange}
+        value={inputs?.plMaxHeight}
+        onInput={(e) => {
+          e.target.value = Math.max(0, parseInt(e.target.value))
+            .toString()
+            .slice(0, 8);
+        }}
+        min={0}
+        type="number"
+      />
+      <TextField
+        name="p1MaxHeight"
+        label="Max P1 Height"
+        variant="outlined"
+        onChange={handleChange}
+        value={inputs?.p1MaxHeight}
+        onInput={(e) => {
+          e.target.value = Math.max(0, parseInt(e.target.value))
+            .toString()
+            .slice(0, 8);
+        }}
+        min={0}
+        type="number"
+      />
+      <div className={classes.itemPalletConfig}>
+        <FormLabel component="legend">Pallet Config</FormLabel>
+        <RadioGroup
+          aria-label="pallet-config"
+          name="palletStatus"
+          value={inputs.palletStatus}
+          onChange={handleChange}
+        >
+          <FormControlLabel value="pl" control={<Radio />} label="PL" />
+          <FormControlLabel value="p1" control={<Radio />} label="P1" />
+        </RadioGroup>
+      </div>
+      <Button variant="contained" onClick={onReset}>
+        Reset
+      </Button>
+      <Button variant="contained" color="secondary" type="submit">
+        save
+      </Button>
+    </Paper>
+  );
+};
